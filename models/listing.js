c649db25c62f303d3c69b2f5b1c8b9cd3960142c
b_gutti <b.gutti@spglobal.com>
2024-05-27 08:02:11 -0400

    Add project files.

---
+++ models/listing.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required:true,
+    },
+    description: String,
+    image: {
+        url: {
+            type: String,
+            default: "https://unsplash.com/photos/a-man-holding-a-surfboard-standing-on-top-of-a-beach-1pcMSzyNPyg",
+            set:(v) => v === "" ? "https://unsplash.com/photos/a-man-holding-a-surfboard-standing-on-top-of-a-beach-1pcMSzyNPyg" :v,
+        },
+        filename: String
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:"Review",
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+});
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;

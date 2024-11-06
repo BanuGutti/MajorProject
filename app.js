135c145edc0da918d1af8bb9f3e93a9f636de82c
b_gutti <b.gutti@spglobal.com>
2024-11-06 01:24:53 -0500

    Add Project Files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -17,14 +18,17 @@ const User = require("./models/users.js");
 const listingRouter = require("./routes/listing.js");
 const reviewsRouter = require("./models/review.js");
 const userRouter = require("./routes/user.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+const dbUrl = process.env.ATLASDB_URL;
+
 main().then(() => {
     console.log("connected to DB");
 }).catch(err => {
     console.log(err);
 });
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 };
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -32,8 +36,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("Error in mongo session store", err);
+});
+
 const sessionOptions = {
-    secret: "mysupesecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -55,9 +73,9 @@ app.use((req, res, next) => {
     res.locals.currUser = req.user;
     next();
 });
-app.get("/", (req, res) => {
-    res.send("Hi I am root");
-});
+//app.get("/", (req, res) => {
+//    res.send("Hi I am root");
+//});
 //app.get("/demouser", async (req, res) => {
 //    let fakeUser = new User({
 //        email: "stud@gmail.com",
